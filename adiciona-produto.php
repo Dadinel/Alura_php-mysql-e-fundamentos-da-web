4ee183454f73665d447aefba2819f44527e4144a
Daniel Mendes De Melo Sousa <daniel.mendes@sp01.local>
2019-02-21 19:06:30 -0300

    Continuação do curso

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,16 +1,23 @@
 <?php include("cabecalho.php"); ?>
-<?php
-$nome = $_GET["nome"];
-$preco = $_GET["preco"];
-$conexao = mysqli_connect('localhost', 'root', '', 'loja');
-$query = "insert into produtos (nome,preco) values ('{$nome}',{$preco});";
-$resultadoDaInsercao = mysqli_query($conexao, $query);
-if(resultadoDaInsercao) { ?>
-    <p class="alert-success">Produto <?=$nome;?> com o preço <?=$preco;?> adicionado com sucesso!</p>
-<?php } else { ?>
-    <p class="alert-danger">Produto <?=$nome;?> não foi adicionado.</p>
-<?php } ?>
+<?php include("conecta.php"); ?>
 
-<?php //mysqli_close($conexao); ?>
+    <?php
+    function insereProduto($conexao, $nome, $preco) {
+        $query = "insert into produtos (nome,preco) values ('{$nome}',{$preco});";
+        return mysqli_query($conexao, $query);
+    }
+
+    $nome = $_GET["nome"];
+    $preco = $_GET["preco"];
+    //$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+    if(insereProduto($conexao, $nome, $preco)) { ?>
+        <p class="text-success">Produto <?=$nome;?> com o preço <?=$preco;?> adicionado com sucesso!</p>
+    <?php
+        } else {
+        $msg = mysqli_error($conexao);
+    ?>
+        <p class="text-danger">Produto <?=$nome;?> não foi adicionado: <?=$msg;?></p>
+    <?php } ?>
 
 <?php include("rodape.php"); ?>
