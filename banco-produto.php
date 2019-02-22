3a344bf41b961c6c42982621ffe44239ae3e31f1
Daniel Mendes De Melo Sousa <daniel.mendes@sp01.local>
2019-02-22 15:30:30 -0300

    Criação da alteração do produto

--- banco-produto.php
+++ banco-produto.php
@@ -33,3 +33,20 @@ function removeProduto($conexao, $id) {
     $query = "delete from produtos where id = '{$id}';";
     return mysqli_query($conexao, $query);
 }
+
+function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria_id, $usado) {
+    $query = "update produtos set nome='{$nome}', preco={$preco}, descricao='{$descricao}',categoria_id={$categoria_id}, usado={$usado} where id = {$id};";
+    return mysqli_query($conexao, $query);
+}
+
+function buscaSoProduto($conexao, $id) {
+    $query = "select * from produtos where id = {$id}";
+    $resultado = mysqli_query($conexao, $query);
+    return mysqli_fetch_assoc($resultado);
+}
+
+function buscaProduto($conexao, $id) {
+    $query = "select p.*, c.nome as categoria_nome from produtos as p join categorias c on p.categoria_id = c.id where p.id = {$id}";
+    $resultado = mysqli_query($conexao, $query);
+    return mysqli_fetch_assoc($resultado);
+}
--- produto-lista.php
+++ produto-lista.php
@@ -23,6 +23,9 @@
             <td><?=$produto['usado'] ? "Sim" : "Não";?></td>
             <td><?=$produto['categoria_nome'];?></td>
             <td>
+                <a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto['id'];?>">Alterar</a>
+            </td>
+            <td>
                 <form action="remove-produto.php" method="post">
                     <input type="hidden" name="id" value="<?=$produto['id'];?>">
                     <button class="btn btn-danger">Remover</button>
